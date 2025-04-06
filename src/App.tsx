bfcf7e3cfa3dc8c284fec047111d4ffda6fe490f
Marine Ligny <mligny.dev@gmail.com>
2025-04-06 11:23:04 +0200

    debut creation section 'continuez vos achats' et modif du css du bouton like pour qu'il soit identique partout car utilisé plusieurs fois

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,51 @@
 import Banner from './components/Banner';
+import CategoriesContainer from './components/Categories';
 import ProductsContainer from './components/ProductsContainer';
 
-
+import { MapPin } from 'lucide-react';
 
 function App() {
   // biome-ignore lint/complexity/noUselessLoneBlockStatements: <explanation>
   {/*ici a modifier pour afficher autant de catégorie qu'il y aura de svg et son title*//*ici a modifier pour afficher autant de catégorie qu'il y aura de svg et son title*/}
-  const categories = Array.from({ length: 6 });
+  const shopping = Array.from({ length: 6 });
   return (
     
     <div>
       <main className='sections'>
         <Banner />
+        <CategoriesContainer />
 
-        <section className="container categories">
-          <div className="categories_text">
-            <h2 className='section-title'>Catégories Recommandées</h2>
-            <p className="text-body">Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptatum ipsam labore</p>
-          </div>
+        <section className="container resume-shopping">
+          <h2 className="section-title">Continuez vos achats</h2>
 
-          <div className="categories-grid">
-            {categories.map((_, index) => (
-              <a 
-                href="/#" 
-                className='category-link'
-                key={index}>
-                  <span className="category-link_icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shirt-icon lucide-shirt" aria-hidden="true">
-                      <path d="M20.38 3.46 16 2a4 4 0 0 1-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z"/>
-                    </svg>
-                  </span>
-                  Mode
-              </a>
-            ))}
+          <div className="slider-products">
+            {shopping.map((_, index) => (
+              <article className="product-card-full" key={index}>
+                <img src="https://picsum.photos/270/358" alt="" className='product-card-full_image'/>
+                <div className="location">
+                  <MapPin />
+                  Montpellier, 34000
+                </div>
+                <div className="product-card-full_body">
+                  <a rel="stylesheet" href="product.html" className='product-card-full_title'> 
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                  </a>
+                  <div className='product-card-full_price'>
+                    15.00€
+                  </div>
+                </div>
+                <button 
+                className='btn-like'
+                type='button'
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-heart-icon lucide-heart" aria-hidden="true">
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/>
+                  </svg>
+                </button>
+              </article>
+
+            ))}  
           </div>
-          
         </section>
 
         <ProductsContainer />

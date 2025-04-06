bca7baa00b11e670aed7b04c42f4dd58d283e616
Marine Ligny <mligny.dev@gmail.com>
2025-04-06 10:17:01 +0200

    legere modification de la banniere avec un display flex pour afficher le background dans la taille definit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-//import { useState } from 'react'
-//import './App.css'
-//import { MapPin } from 'lucide-react';
-//import CardProduct from './components/CardProduct';
 import Banner from './components/Banner';
 import ProductsContainer from './components/ProductsContainer';
-//import { Link } from 'react-router-dom';
+
 
 
 function App() {
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,7 @@
 function Banner() {
 
     return (
+      <div className="banner_container">
         <section className="banner">
           <h1 className='banner_title'>Donnez une seconde vie à vos achats</h1>
           <p className='banner_description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur eum voluptas quod alias vitae facilis quidem minima commodi placeat nulla, officiis ducimus fuga eveniet enim quos. Consequuntur esse quod in?</p>
@@ -11,6 +12,7 @@ function Banner() {
           className='btn-filled'
           >Commencer à vendre</a>
         </section>
+      </div>
     )
   }
   export default Banner;

bf2ea0a507c0f08b7c8e277d381d716802e39f98
Marine Ligny <mligny.dev@gmail.com>
2025-04-10 15:59:06 +0200

    creation de la barre header pour mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 
 import Banner from './components/Banner';
 import CategoriesContainer from './components/Categories';
+import Header from './components/Header';
 import Posts from './components/Posts';
 import ProductsContainer from './components/ProductsContainer';
 import SliderProducts from './components/SliderProducts';
@@ -10,8 +11,10 @@ import SliderProducts from './components/SliderProducts';
 function App() {
  
   return (
-    
     <div>
+
+      <Header />
+
       <main className='sections'>
         <Banner />
         <CategoriesContainer />
@@ -19,8 +22,18 @@ function App() {
         <ProductsContainer />
         <Posts />
       </main>
-    </div>
-    
+
+      <aside className='mobile-menu'>
+        <button className="btn-icon" type='button'title='Fermer le menu'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x-icon lucide-x" aria-hidden="true">
+          <path d="M18 6 6 18"/>
+          <path d="m6 6 12 12"/>
+          </svg>
+        </button>
+
+      </aside>
+
+    </div> 
   )
 }
 

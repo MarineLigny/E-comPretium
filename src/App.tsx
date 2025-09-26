a160355779b3835558386b40fc6853815045bd7c
Marine Ligny <mligny.dev@gmail.com>
2025-09-26 16:06:10 +0200

    update css city in product.css

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,22 @@ import SliderProducts from './components/SliderProducts';
 
 
 function App() {
- 
+
   return (
     <div>
 
       <Header />
+      <header className="Navbar container">
+        <form action="" className='navbar-search'>
+          <input type="search" placeholder="Rechercher un produit, une marque..." aria-label="Rechercher un produit, une marque..." />
+          <button type="submit" title='recherche'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search-icon lucide-search" aria-hidden="true">
+              <circle cx="11" cy="11" r="8" />
+              <path d="m21 21-4.3-4.3" />
+            </svg>
+          </button>
+        </form>
+      </header>
 
       <main className='sections'>
         <Banner />
@@ -26,7 +37,7 @@ function App() {
 
       <MobileMenu />
 
-    </div> 
+    </div>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,22 @@
 function Header() {
     return (
         <header className="Navbar container">
-        <a href="menu.html" className="btn-icon" title="ouvrir le menu">
+        <a href="menu.html" className="btn-icon" title="Ouvrir le menu" aria-label="Ouvrir le menu">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-align-center-icon lucide-align-center" aria-hidden="true">
           <path d="M17 12H7"/>
           <path d="M19 18H5"/>
           <path d="M21 6H3"/>
           </svg>
         </a>
+        
         <a href="/index.html" className='mobile-menu_logo'>Pretium</a>
-        <button className="btn-icon" type='button' title="rechercher">
+
+        <a href="search.html" className="btn-icon"  title="Rechercher" aria-label="Rechercher">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search-icon lucide-search" aria-hidden="true">
           <circle cx="11" cy="11" r="8"/>
           <path d="m21 21-4.3-4.3"/>
         </svg>
-        </button>
+        </a>
       </header>
     )
 }
